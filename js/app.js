d569b5bac909379b19ff82365a8008b4a60fc1bb
Damilare Richard Olagbende <damilareshinaayo@gmail.com>
2020-07-02 21:45:28 +0100

    ConstructionHNG

--- js/app.js
+++ js/app.js
@@ -9,6 +9,69 @@ function onClickMenu(){
 
 }
 
+const carouselSlider = document.querySelector('.slider');
+const carouselImages = document.querySelectorAll('.slider img');
+
+
+
+const prevBtn = document.querySelector('#prev-btn');
+const nextBtn = document.querySelector('#next-btn');
+
+
+// let counter = 0;
+// const size = carouselImages[0].clientWidth;
+
+// carouselSlider.style.transform = 'translateX(' + (-size * counter) + 'px)'; 
+
+// nextBtn.addEventListener('click',()=> {
+// 		if (counter >= carouselImages.length - 1) return;
+// 		carouselSlider.style.transition = 'transform 0.5s ease';
+// 		counter++;
+
+// 		carouselSlider.style.transform = 'translateX(' + (-size * counter ) + 'px)';
+
+// 	});
+
+// 	prevBtn.addEventListener('click', () => {
+// 		if (counter = 0) return;
+// 		carouselSlider.style.transition = 'transform 0.5s ease';
+// 		counter--;
+
+// 		carouselSlider.style.transform = 'translateX(' + (-size * counter ) + 'px)';
+
+// 	});
+
+	// carouselSlider.addEventListener('transitionend', () => {
+	// 	if (carouselImages[counter].id === '') {
+	// 		carouselSlider.style.transition ='none';
+	// 		counter = carouselImages.length - 2;
+	// 		carouselSlider.style.transform = 'translateX(' + (-size * counter ) + 'px)';
+
+	// 	}
+
+	// 	if (carouselImages[counter].id === '') {
+	// 		carouselSlider.style.transition ='none';
+	// 		counter = carouselImages.length - counter;
+	// 		carouselSlider.style.transform = 'translateX(' + (-size * counter ) + 'px)';
+
+	// 	}
+	// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // const scroll = new SmoothScroll('.nav-links a[href*="#"]',{
 // 	speed: 300 
 
